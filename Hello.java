082e67a8d896e25a226fbb9c02453aaa169581c6
Mohammad Al Hadi Jaafar Jouni <mjjouni@umich.edu>
2024-02-10 18:45:19 -0500

    initial commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
+// this is the forked branch
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("This is in a new Branch");
+        System.out.println("This is in a new Branch1");
         for(int i=1;i<=10;i++) {
             System.out.println("Hello World... This is the "+i +"th iteration");
         }
